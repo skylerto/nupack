34c78eb18476c44f561f6aedbf66a340fcbadecb
Skyler Layne <skylerclayne@gmail.com>
2016-05-18 16:59:40 -0400

    Write specification for Project class

--- spec/nupack_spec.rb
+++ spec/nupack_spec.rb
@@ -1,1 +1,17 @@
 require 'nupack'
+
+describe Nupack::Project do
+  it 'should create a new Project with a cost of $1000, 1 person and type "other"' do
+    project = Nupack::Project.new(1000)
+    expect(project.cost).to eq(1000)
+    expect(project.people).to eq(1)
+    expect(project.type).to eq(:other)
+  end
+
+  it 'should create a new Project with a cost of $1000, 4 people, and type "food"' do
+    project = Nupack::Project.new(1000, 4, "food")
+    expect(project.cost).to eq(1000)
+    expect(project.people).to eq(4)
+    expect(project.type).to eq(:food)
+  end
+end
