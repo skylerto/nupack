8d62cd15cb79ba4ab3d528143fd3b2905c75fd0d
Skyler Layne <skylerclayne@gmail.com>
2016-05-18 18:18:55 -0400

    Quick implementation on estimate method

--- lib/nupack/calculator.rb
+++ lib/nupack/calculator.rb
@@ -10,5 +10,14 @@ module Nupack
     def cost(price, markup)
       price.is_a?(Numeric) ? price * @markups.fetch(markup, 0) : 0
     end
+
+    def estimate(project)
+      [
+        project.cost,
+        flat = cost(project.cost, :flat),
+        cost(flat, :person),
+        cost(flat, project.type)
+      ].reduce(:+).round(2)
+    end
   end
 end
